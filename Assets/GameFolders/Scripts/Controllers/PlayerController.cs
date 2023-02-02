9cda0e1d24cdc6c295cd872d650dedbfa550446b
ikbal arslan <120676706+ikbalarslann@users.noreply.github.com>
2023-02-02 06:21:58 +0300

    jump

--- Assets/GameFolders/Scripts/Controllers/PlayerController.cs
+++ Assets/GameFolders/Scripts/Controllers/PlayerController.cs
@@ -5,6 +5,12 @@ namespace FirstGame.Controllers
 {
     public class PlayerController : MonoBehaviour
     {
+        Rigidbody2D _rigidbody2D;
+
+        private void Awake()
+        {
+            _rigidbody2D= GetComponent<Rigidbody2D>();  
+        }
         // Start is called before the first frame update
         void Start()
         {
@@ -15,7 +21,13 @@ namespace FirstGame.Controllers
         void Update()
         {
             //transform.Translate(Vector3.right * 0.01f); this script just for practising.
+            //transform.position += Vector3.right * 0.01f; still practising.
+            if (Input.GetMouseButtonDown(0))
+            {
+                _rigidbody2D.AddForce(Vector3.up * 300f);
+            }            }
+            
         }
     }
 
-}
+
